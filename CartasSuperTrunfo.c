36372bb7dce94f8b35d6b3bb0d52305f5e8f0147
NathaliaAguiart <programacao_nathaliaaguiar@yahoo.com>
2025-04-04 22:39:22 +0000

    CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 
 int main(){
-    printf("Desafio Super Trunfo!\n");
-    printf("Novo Comit\n");
+
+    char estado_1;
+    char cod_carta [5] = 01;
+    char nome_cidade [10] = rio;
+    int populacao_6729894;
+    float area_km_1200330;
+    float pib_5307823;
+    int ponto_turistico_58;
+
+    printf("digite seu estado:");
+    scanf("%s",&estado_1);
+    printf("o estado é: %s\n", estado_1);
+
+    printf("Cod da carta:");
+    scanf("%c",&cod_carta);
+    printf("o cod carta é:");
+
+    printf("nome da cidade:");
+    scanf("%s",&nome_cidade);
+    printf("a cidade é:");
+
+    printf("populacao");
+    scanf("%d",&populacao);
+    printf("a populacao é:");
+
+    printf("area em km");
+    scanf("%f",&area_km);
+    printf("a area em km é:");
+
+    printf("digite o pib");
+    scanf("%f",&pib);
+    printf("o pib é:");
+
+    printf("ponto turistico");
+    scanf("%d",&ponto_turistico);
+    printf("o ponto turistico é:");
+
+
+
+
+
+
 
 }
